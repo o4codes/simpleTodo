3efc637527488a183ea96c8dede2dfccf2aac0a2
shadrach <oforkansi.shadrach@gmail.com>
2022-01-27 12:28:12 +0100

    token auth added

--- core/urls.py
+++ core/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('rest_framework.urls')),
+    path('auth/', views.obtain_auth_token),
     path('api/v1/', include('todo.urls'))
+    
 ]
