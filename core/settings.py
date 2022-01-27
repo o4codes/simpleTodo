263b62abb1c315b4dc691bc23fa7e4eff0f96b8e
shadrach <oforkansi.shadrach@gmail.com>
2022-01-27 11:53:06 +0100

    todo serializers created

--- core/settings.py
+++ core/settings.py
@@ -55,6 +55,14 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'core.urls'
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
